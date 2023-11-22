b48ef3939be1ec1f03383253254a9a200e0b463a
Franklina Addae <franklinaaddae306@gmail.com>
2023-11-22 18:08:44 +0000

    Initial commit

--- script.js
+++ script.js
@@ -1,83 +1,41 @@
-window.addEventListener('scroll', function() {
-    const header = document.getElementById('mainHeader');
-    const headerContent = document.getElementById('headerContent');
-    const scrollPos = window.scrollY;
-    
-    if (scrollPos > 20) {
-      header.style.height = '100px';
-      headerContent.style.height = '50px';
-    } else {
-      header.style.height = '150px';
-      headerContent.style.height = '60px';
-    }
-  });
-
-  const hamburgerMenu = document.getElementById('hamburgerMenu');
-const menuOverlay = document.getElementById('menuOverlay');
-const hamburgerContainer = document.getElementById('hamburgerContainer');
-const menuText = document.getElementById('menu-text');
-
-function toggleMenu() {
-hamburgerMenu.classList.toggle('active');
-menuOverlay.style.height = menuOverlay.style.height === '100vh' ? '0' : '100vh';
-menuText.textContent = menuText.textContent === 'Menu' ? 'Close' : 'Menu';
-}
-
-// Click event for hamburgerMenu
-hamburgerMenu.addEventListener('click', function(event) {
-event.stopPropagation();
-toggleMenu();
-});
-
-hamburgerContainer.addEventListener('click', function() {
-toggleMenu();
-});
-
-document.addEventListener('click', function(event) {
-if (!menuOverlay.contains(event.target) && event.target !== hamburgerContainer) {
-  hamburgerMenu.classList.remove('active');
-  menuOverlay.style.height = '0';
-  menuText.textContent = 'Menu';
-}
-});
-
-  // Function to scroll to the top smoothly
-  function scrollToTop() {
-    const scrollStep = -window.scrollY / (800 / 15); 
-
-    // Function to scroll the page smoothly
-    function scrollStepFunction() {
-      if (window.scrollY !== 0) {
-        window.scrollBy(0, scrollStep);
-        requestAnimationFrame(scrollStepFunction);
+document.addEventListener('DOMContentLoaded', function () {
+    var page2Header = document.querySelector('.page2-header');
+    var page2 = document.querySelector('.page2');
+  
+    window.addEventListener('scroll', function () {
+      var scrollPosition = window.scrollY;
+      var page2Position = page2.offsetTop;
+  
+      if (scrollPosition >= page2Position) {
+        page2Header.classList.add('visible');
+      } else {
+        page2Header.classList.remove('visible');
       }
+    });
+  });
+  
+  
+  window.addEventListener('scroll', function() {
+    var scrollToTopBtn = document.getElementById('scrollToTopBtn');
+    if (window.scrollY > 1000) { 
+      scrollToTopBtn.classList.add('show');
+    } else {
+      scrollToTopBtn.classList.remove('show');
     }
-
-    requestAnimationFrame(scrollStepFunction);
-  }
-
-  const scrollToTopDiv = document.querySelector('.top-arrow-section');
-  scrollToTopDiv.addEventListener('click', scrollToTop);
-const scrollToTopButtonDuplicate = document.getElementById('scrollToTopButtonDuplicate');
-scrollToTopButtonDuplicate.addEventListener('click', scrollToTop);
-
-document.getElementById('contactLink').addEventListener('click', function (e) {
-  e.preventDefault();
-  const scrollDistance = 2900;
-
-  window.scroll({
-      top: scrollDistance,
-      behavior: 'smooth'
   });
-});
-
-
-document.getElementById('shortCoursesLink').addEventListener('click', function (e) {
-  e.preventDefault();
-  const scrollDistance = 1170;
-
-  window.scroll({
-      top: scrollDistance,
+  document.getElementById('scrollToTopBtn').addEventListener('click', function(e) {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
       behavior: 'smooth'
+    });
   });
-});
+
+  
+
+  function scrollToDiv(className) {
+    var element = document.querySelector('.' + className);
+    if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+    }
+}
