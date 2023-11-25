04fac8b277ed6793aeeca154c9dfd00375d35676
Franklina Addae <franklinaaddae306@gmail.com>
2023-11-25 13:58:05 +0000

    Added FAQ page

--- script.js
+++ script.js
@@ -39,3 +39,57 @@ document.addEventListener('DOMContentLoaded', function () {
         element.scrollIntoView({ behavior: 'smooth' });
     }
 }
+
+
+window.addEventListener('scroll', function() {
+  var scrollToTopBtn = document.getElementById('scrollToTopBtn');
+  if (window.scrollY > 400) { 
+    scrollToTopBtn.classList.add('show');
+  } else {
+    scrollToTopBtn.classList.remove('show');
+  }
+});
+document.getElementById('scrollToTopBtn').addEventListener('click', function(e) {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+
+
+
+document.getElementById('application').addEventListener('click', function (e) {
+e.preventDefault();
+const scrollDistance = 400;
+
+window.scroll({
+    top: scrollDistance,
+    behavior: 'smooth'
+});
+});
+
+document.getElementById('requirement').addEventListener('click', function (e) {
+e.preventDefault();
+const scrollDistance = 1400;
+
+window.scroll({
+    top: scrollDistance,
+    behavior: 'smooth'
+});
+});
+
+
+window.addEventListener('scroll', function() {
+  const header = document.getElementById('mainHeader');
+  const headerContent = document.getElementById('headerContent');
+  const scrollPos = window.scrollY;
+  
+  if (scrollPos > 20) {
+    header.style.height = '90px';
+    headerContent.style.height = '100px';
+  } else {
+    header.style.height = '100px';
+    headerContent.style.height = '130px';
+  }
+});
